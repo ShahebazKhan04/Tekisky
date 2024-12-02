a12d0dcad2a49384b0b697b954d7d79817149543
ShahebazKhan04 <pathanshahbazkhan1433@gmail.com>
2024-12-02 16:03:05 +0530

    made student curd in batch array

--- controllers/batchController.js
+++ controllers/batchController.js
@@ -1,8 +1,12 @@
+import batchModel from "../models/batchModel.js";
+import staffModel from "../models/staffModel.js";
 import {
+  addStudentsToBatchServices,
   createBatchServices,
   deleteBatchServices,
   getAllBatchesServices,
   getSingleBatchServices,
+  updatebatchServices,
 } from "../services/batchServices.js";
 
 export const createBatchController = async (req, res) => {
@@ -112,6 +116,168 @@ export const deleteBatchController = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "error while deleting batch",
+      error: error.message,
+    });
+  }
+};
+
+export const addStudentsToBatchController = async (req, res) => {
+  try {
+    const batchId = req.params.id;
+    const { studentIds } = req.body;
+
+    if (studentIds.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Provide a valid array of student IDs.",
+      });
+    }
+
+    const batch = await addStudentsToBatchServices(batchId);
+    if (!batch) {
+      return res.status(400).json({
+        success: false,
+        message: "please provide a array pf students",
+      });
+    }
+
+    const alreadyPresent = studentIds.filter((id) =>
+      batch.students.includes(id)
+    );
+    if (alreadyPresent.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: `The following students are already in the batch`,
+      });
+    }
+
+    batch.students.push(...studentIds);
+    await batch.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Students added successfully.",
+      data: batch,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "error while adding students to batch",
+      error: error.message,
+    });
+  }
+};
+
+export const updatebatchController = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const {
+      batchName,
+      subject,
+      trainer,
+      startDate,
+      endDate,
+      capacity,
+      status,
+      timeSchedule,
+    } = req.body;
+
+    const batch = await updatebatchServices(id, {
+      batchName,
+      subject,
+      trainer,
+      startDate,
+      endDate,
+      capacity,
+      status,
+      timeSchedule,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "batch updated successfully",
+      batch: batch,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "error while upadting batch controller",
+      error: error.message,
+    });
+  }
+};
+
+export const updateStudentToBatchController = async (req, res) => {
+  try {
+    const batchId = req.params.id;
+    const { studentIds } = req.body;  
+    if (studentIds.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Provide a valid array of student IDs.",
+      });
+    }
+
+    const batch = await batchModel.findByIdAndUpdate(
+      batchId,
+      { students: studentIds },
+      { new: true, runValidators: true }
+    );
+
+    if (!batch) {
+      return res.status(404).json({
+        success: false,
+        message: "Batch not found.",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "batch updated successfully",
+      updatedBatch: batch,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "error while upadting batch controller",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteSingleStudentFromBatchController = async (req, res) => {
+  try {
+    const batchId = req.params.id;
+    const { studentId } = req.body;
+
+    if (!studentId) {
+      return res.status(400).json({
+        success: false,
+        message: "Provide a valid student ID to delete.",
+      });
+    }
+
+    const batch = await batchModel.findByIdAndUpdate(
+      batchId,
+      { $pull: { students: studentId } },
+      { new: true }
+    );
+
+    if (!batch) {
+      return res.status(404).json({
+        success: false,
+        message: "Batch not found.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Student removed successfully.`,
+      updatedBatch: batch,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error while removing student from batch.",
       error: error.message,
     });
   }
--- routes/batchRoutes.js
+++ routes/batchRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { authStaff } from "../middlewares/auth.js";
 import {
+  addStudentsToBatchController,
   createBatchController,
   deleteBatchController,
+  deleteSingleStudentFromBatchController,
   getAllBatchesController,
   getSingleBatchController,
+  updatebatchController,
+  updateStudentToBatchController,
 } from "../controllers/batchController.js";
 const batchRoute = express.Router();
 
@@ -12,4 +16,8 @@ batchRoute.post("/create", authStaff, createBatchController);
 batchRoute.get("/all", authStaff, getAllBatchesController);
 batchRoute.get("/:id", authStaff, getSingleBatchController);
 batchRoute.delete("/delete/:id", authStaff, deleteBatchController);
+batchRoute.put("/:id/add-students", authStaff, addStudentsToBatchController);
+batchRoute.put("/update/:id", authStaff, updatebatchController);
+batchRoute.put("/:id/update-students", authStaff, updateStudentToBatchController);
+batchRoute.put("/:id/delete-student", authStaff, deleteSingleStudentFromBatchController);
 export default batchRoute;
